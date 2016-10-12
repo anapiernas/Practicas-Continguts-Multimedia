1ffb0eaa8726234b4098c544a11bd8644c9019fe
anapiernas <anapiernas1@gmail.com>
2016-10-12 23:55:48 +0200

    Add files via upload

---
+++ mdesp.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('ul li:has(ul)').hover(function(e) {
+         $(this).find('ul').css({display: "block"});
+     },
+     function(e) {
+         $(this).find('ul').css({display: "none"});
+     });
+});
